2ac2727daf3916d77e629918442caade53b4e43b
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-10-14 13:32:11 +0530

    jgnjg

--- Frontend/src/components/JobsComponents/JobDescrition.jsx
+++ Frontend/src/components/JobsComponents/JobDescrition.jsx
@@ -23,7 +23,7 @@ const JobDescription = () => {
 
     const applyJobHandler = async () => {
         try {
-            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`);
+            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, { withCredentials: true });
 
             if (res.data.success) {
                 setIsApplied(true); // Update the local state
