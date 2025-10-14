dc1ce4349b1635b13a78bc2702e7d4fea786ed16
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-10-14 12:29:19 +0530

    backend and fronted cors updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express"
+import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./utils/db.js";
@@ -12,24 +12,24 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials:true
-}
+  origin: "https://get-hired-brown.vercel.app/",
+  credentials: true,
+};
 app.use(cors(corsOptions));
-app.get("/",(req,res)=>{
-    res.send("API is running");
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/company", companyRoutes);
 app.use("/api/v1/jobs", jobRoutes);
 app.use("/api/v1/applications", applicationRoutes);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server is running on ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on ${PORT}`);
+});
