587032c9197002907c3702f877b069f0f925cc30
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-11-10 09:44:08 +0530

    new backend and updated the backend api

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -4,10 +4,18 @@
 //   "http://localhost:8000/api/v1/applications";
 // export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
 export const USER_API_END_POINT =
-  "https://gethired-1467.onrender.com/api/v1/user";
+  "https://get-hired-backend.vercel.app/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://gethired-1467.onrender.com/api/v1/jobs";
+  "https://get-hired-backend.vercel.app/api/v1/jobs";
 export const APPLICATION_API_END_POINT =
-  "https://gethired-1467.onrender.com/api/v1/applications";
+  "https://get-hired-backend.vercel.app/api/v1/applications";
 export const COMPANY_API_END_POINT =
-  "https://gethired-1467.onrender.com/api/v1/company";
+  "https://get-hired-backend.vercel.app/api/v1/company";
+// export const USER_API_END_POINT =
+//   "https://gethired-1467.onrender.com/api/v1/user";
+// export const JOB_API_END_POINT =
+//   "https://gethired-1467.onrender.com/api/v1/jobs";
+// export const APPLICATION_API_END_POINT =
+//   "https://gethired-1467.onrender.com/api/v1/applications";
+// export const COMPANY_API_END_POINT =
+//   "https://gethired-1467.onrender.com/api/v1/company";
