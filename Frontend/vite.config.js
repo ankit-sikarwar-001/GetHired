130fdfc5a228418580346f2338b1cb7c33ebaa8e
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-11-12 17:37:36 +0530

    update frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,16 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ Ensures correct path resolution on deployment
   server: {
-    host: true, // or use '0.0.0.0'
-    port: 5173, // default port or change if needed
+    host: true,
+    port: 5173,
   },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // default for Vite
   },
 });
